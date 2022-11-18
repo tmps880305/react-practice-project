759557510a2c1ac0cd2cfe7fa7e333b473b37ec4
randy <tmps880305@gmail.com>
2022-11-17 21:17:39 -0500

    Section 9-104. Working with Portals Use ReactDOM portal to prevent backdrop/overlay to block the entire screen
    1. ReactDOM.createPortal
    2. <Backdrop />
    3. Careful about the function in the component (eg. <Backdrop onCanceled={props.onCanceled})

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,27 +1,42 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import Card from './Card'
 import Button from './Button'
 import classes from './ErrorModal.module.css'
 
+const Backdrop = props => {
+    return <div className={classes.backdrop} onClick={props.onCanceled}/>;
+};
+
+const ModalOverlay = props => {
+    return (<Card className={classes.modal}>
+
+        <header className={classes.header}>
+            <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+            <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+            <Button onClick={props.onCanceled}>Got it</Button>
+        </footer>
+
+    </Card>);
+};
+
+
 const ErrorModal = props => {
 
     return (
         <>
-            <div className={classes.backdrop} onClick={props.onCanceled}/>
-            <Card className={classes.modal}>
-
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={props.onCanceled}>Got it</Button>
-                </footer>
-
-            </Card>
+            {ReactDOM.createPortal(<Backdrop onCanceled={props.onCanceled}/>, document.getElementById('backdrop-root'))}
+            {ReactDOM.createPortal(<ModalOverlay
+                    title={props.title}
+                    message={props.message}
+                    onCanceled={props.onCanceled}
+                />,
+                document.getElementById('overlay-root'))}
         </>
     )
 };
